fe7c3cf7d9074ff12e1155fe007fd56431ee70e2
Mohamed Saeed <96616586+mid0o@users.noreply.github.com>
2024-01-02 04:49:44 +0200

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,21 +1,31 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if there si a cycle
- * @list: pointer to a list
- * Return: 1 if there is 0 otherwise
- */
+ * check_cycle - checks if there is a cycle
+ * @list: list to be checked
+ * Return: 0 if no cycle, 1 if it does have
+*/
+
 int check_cycle(listint_t *list)
 {
-	listint_t *tmp;
-	if (list == NULL)
-		return (0);
-	tmp = list;
-	while (tmp->next != NULL)
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
 	{
-		if (tmp->next == list)
-			return (1);
-		tmp = tmp->next;
+		return (0);
 	}
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+
 	return (0);
 }
