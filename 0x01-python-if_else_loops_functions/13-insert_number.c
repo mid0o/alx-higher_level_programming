4f4c9378d76b3ce9b860a6f0bc9b5a0a5b7c8117
Mohamed Saeed <96616586+mid0o@users.noreply.github.com>
2024-01-04 04:41:40 +0200

    Update 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,27 +1,43 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <unistd.h>
+
 /**
- * insert_node - adds node in a specific position
- * @head: pointer to a struct
- * @number: number to insert
- * Return: @ of new node else NULL if it failed
+ * insert_node - function to insert an element in ordered list
+ * @head: pointer to the pointer of the first element of the list
+ * @number: node to insert a new node
+ *
+ * Return: Address of an inserted node or NUL
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-listint_t *tmp, *q = NULL, *new;
-if (*head == NULL)
-	return NULL;
-new = (listint_t *)malloc(sizeof(listint_t));
-if (new == NULL)
-	return NULL;
-new->n = number;
-new->next = NULL;
-tmp = *head;
-while (tmp != NULL && number > tmp->n)
-{
-	q = tmp;
-	tmp = tmp->next;
-}
-new->next = q->next;
-q->next = new;
-return (new);
+	listint_t *current_node = *head;
+	listint_t *new_node = NULL;
+	listint_t *temp = NULL;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = number;
+	new_node->next = NULL;
+
+	if (!*head || (*head)->n > number)
+	{
+		new_node->next = *head;
+		return (*head = new_node);
+	}
+	else
+	{
+		while (current_node && current_node->n < number)
+		{
+			temp = current_node;
+			current_node = current_node->next;
+		}
+		temp->next = new_node;
+		new_node->next = current_node;
+	}
+	return (new_node);
 }
